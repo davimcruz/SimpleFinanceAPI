3adfded5b21ca8669746fd1ea7cc9af1d3aa1de0
Davi <davimachadocruz@gmail.com>
2024-04-17 00:55:40 -0300

    Atualização do CORS

--- pages/api/Auth/login.tsx
+++ pages/api/Auth/login.tsx
@@ -10,6 +10,17 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // Adicionar cabeçalhos de CORS
+  res.setHeader("Access-Control-Allow-Origin", "https://simplefinance.cloud") // Substitua pelo domínio correto do seu front-end
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  res.setHeader("Access-Control-Allow-Credentials", "true")
+
+  // Lidar com o pedido de preflight do CORS
+  if (req.method === "OPTIONS") {
+    return res.status(200).end()
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método não permitido" })
   }
