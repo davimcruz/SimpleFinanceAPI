5c359ea93860fb38f594ceae9517efbbc0ce16aa
Davi <davimachadocruz@gmail.com>
2024-04-17 01:32:10 -0300

    Atualização do CORS

--- pages/api/Auth/login.tsx
+++ pages/api/Auth/login.tsx
@@ -6,20 +6,14 @@ import { dbConfig } from "@/config/dbConfig"
 
 const pool = mysql.createPool(dbConfig)
 
-const allowedOrigins = "*"
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const origin = req.headers.origin
-
-  if (origin && allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin)
-    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS")
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
-    res.setHeader("Access-Control-Allow-Credentials", "true")
-  }
+  res.setHeader("Access-Control-Allow-Origin", "*") 
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  res.setHeader("Access-Control-Allow-Credentials", "true")
 
   if (req.method === "OPTIONS") {
     return res.status(200).end()
